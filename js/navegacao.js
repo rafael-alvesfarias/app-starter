10eba01ccfed3db0fb495b82e70d4933aaf6317e
Rafael Alves Farias <rafael.alvesfarias@outlook.com>
2015-02-24 21:35:37 -0300

    Refatoração da navegação

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,55 +1,88 @@
 var selecionado;
-var primeiroElemento = $("#app-1");
+
+Navegacao = function(primeiroElemento, seletores){
+	
+	for(i in seletores){
+		$(seletores[i]).each(function(index){
+			if(index == 0){
+				$(this).attr("primeiroDoSelector");
+			}
+			$(this).attr("horizontal-index", index);
+			$(this).attr("vertical-index", i);
+		});
+	}
+	
+	
+	//TODO montar a navegação para os elementos encontrados pelo seletor
+	
+	
+	this.navegar = function(event){
+		switch(event.keyCode){
+			case 37: 
+				if(selecionado){
+					indiceVertical = parseInt(selecionado.attr("vertical-index"));
+					indiceHorizontal = parseInt(selecionado.attr("horizontal-index"));
+					elementoAnterior = $("*[horizontal-index='" + (indiceHorizontal - 1) + "'][vertical-index='" + indiceVertical + "']");
+					if(elementoAnterior.length){
+						selecionar(elementoAnterior);
+					}
+				}else{
+					selecionar(primeiroElemento);
+				}
+			break;
+			case 38:
+				if(selecionado){
+					indiceVertical = parseInt(selecionado.attr("vertical-index"));
+					indiceHorizontal = parseInt(selecionado.attr("horizontal-index"));
+					elementoSuperior = $("*[vertical-index='" + (indiceVertical - 1) + "'][horizontal-index='0']");
+					if(elementoSuperior.length){
+						selecionar(elementoSuperior);
+					}
+				}else{
+					selecionar(primeiroElemento);
+				}
+			break;
+			case 39:
+				if(selecionado){
+					indiceVertical = parseInt(selecionado.attr("vertical-index"));
+					indiceHorizontal = parseInt(selecionado.attr("horizontal-index"));
+					proxElemento = $("*[horizontal-index='" + (indiceHorizontal + 1) + "'][vertical-index='" + indiceVertical + "']");
+					if(proxElemento.length){
+						selecionar(proxElemento);
+					}
+				}else{
+					selecionar(primeiroElemento);
+				}
+			break;
+			case 40:
+				if(selecionado){
+					indiceVertical = parseInt(selecionado.attr("vertical-index"));
+					indiceHorizontal = parseInt(selecionado.attr("horizontal-index"));
+					elementoInferior = $("*[vertical-index='" + (indiceVertical + 1) + "'][horizontal-index='0']");
+					if(elementoSuperior.length){
+						selecionar(elementoInferior);
+					}
+				}else{
+					selecionar(primeiroElemento);
+				}
+			break;
+			case 13:
+				if(selecionado){
+					selecionado.click();
+				}
+			break;
+		}
+	}
+}
 
 function navegar(event){
 	
-	if(event.keyCode == 13){
-		if(selecionado){
-			selecionado.click();
-		}
-		return;
-	}
-	
-	if(!selecionado){
-		selecionar(primeiroElemento);
-	}
-	
-	switch(event.keyCode){
-		case 37: 
-			arrayId = selecionado.attr("id").split("-");
-			tipo = arrayId[0];
-			indice = parseInt(arrayId[1]);
-			elementoAnterior = $("#" + tipo + "-" + (indice-1));
-			if(elementoAnterior.length){
-				selecionar(elementoAnterior);
-			}
-		break;
-		case 38:
-			if(selecionado.attr("id").split("-")[0] == "app"){
-				selecionar($("#menu-1"));
-			}
-		break;
-		case 39:
-			arrayId = selecionado.attr("id").split("-");
-			tipo = arrayId[0];
-			indice = parseInt(arrayId[1]);
-			proxElemento = $("#" + tipo + "-" + (indice+1));
-			if(proxElemento.length){
-				selecionar(proxElemento);
-			}
-		break;
-		case 40:
-			if(selecionado.attr("id").split("-")[0] == "menu"){
-				selecionar($("#app-1"));
-			}
-		break;
-	}
 }
 
 function selecionar(elemento){
 	if(selecionado){
-		$(selecionado).trigger("perdeuSelecao");
+		selecionado.trigger("perdeuSelecao");
 	}
-	$(elemento).trigger("ganhouSelecao");
+	elemento.trigger("ganhouSelecao");
 	selecionado = elemento;
 }
