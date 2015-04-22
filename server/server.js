6e10710850791ebd8daa319f02a0ff9b792bd3e5
rafaelfarias <rafaelfarias@9.13.142.21>
2015-04-22 14:58:44 -0300

    Controle remoto

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var dao = require("../app/data/dao/aplicativosDAO");
+
+function listarAplicativos(){
+	return dao.listar();
+}
+
+exports.start = function(controleRemoto){
+	http.createServer(function(request, response) {
+		controleRemoto(request.url.replace("/", ""));
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("<h1>Teste</h1>");
+		response.end();
+	}).listen(3000, function() {
+		console.log("server started");
+	});
+};
