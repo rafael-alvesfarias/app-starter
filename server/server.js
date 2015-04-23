3e27a43d875bd7e515dc38337071d49010cf58c5
Rafael Alves Farias <rafael.alvesfarias@outlook.com>
2015-04-23 00:01:45 -0300

    Controle remoto

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var dao = require("../app/data/dao/aplicativosDAO");
+var fs = require("fs");
 
 function listarAplicativos(){
 	return dao.listar();
@@ -8,9 +9,11 @@ function listarAplicativos(){
 exports.start = function(controleRemoto){
 	http.createServer(function(request, response) {
 		controleRemoto(request.url.replace("/", ""));
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write("<h1>Teste</h1>");
-		response.end();
+		fs.readFile("server/controle.html", function(err, data) {
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write("asd");
+			response.end();
+		});
 	}).listen(3000, function() {
 		console.log("server started");
 	});
